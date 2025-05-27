28b77d59c5e199346cd4406f3807e5ac8e845892
SamuelSant10 <samuelss.tj@gmail.com>
2025-05-27 18:04:27 -0400

    Update managers.py

--- app/managers.py
+++ app/managers.py
@@ -1,6 +1,8 @@
-import sqlite3
+from dataclasses import dataclass
 
-from app.models import Actor
+@dataclass
+class Actor:
+    id: int
+    first_name: str
+    last_name: str
 
-
-# add manager here
